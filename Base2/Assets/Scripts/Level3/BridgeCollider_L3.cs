359a6367db95802a74af128d38bd8f33ebe58880
Carakai <lilli.luna@hotmail.de>
2015-06-02 16:17:56 +0200

    bridge collider lvl 3

---
+++ Base2/Assets/Scripts/Level3/BridgeCollider_L3.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeCollider_L3 : MonoBehaviour {
+	
+	public string objectName;
+	
+	void OnTriggerEnter(Collider other) {
+	}
+	
+	void OnTriggerStay(Collider other) {
+		Debug.Log ("onTriggerStay");
+		
+		// if it is the bridge, move the player slightly
+		if (objectName.Equals ("Bridge")) {
+			float randomNumber = Random.Range (-0.1f, 0.00f);
+			Vector3 position = other.gameObject.transform.position;
+			other.gameObject.transform.position = new Vector3 (position.x + randomNumber, position.y, position.z);
+			
+			// if it is the floor below the bridge, move the player back to the beginning
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {	
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
