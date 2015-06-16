797fef1222b356df31806780aa3108a4f846c0fa
Lanelore <bzankl@gmx.at>
2015-06-16 14:45:36 +0200

    SplashScreen

---
+++ Base2/Assets/Scripts/SplashScreen/PlayMovie.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour
+{
+	private MovieTexture mtex;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		/*
+		mtex = gameObject.renderer.material.mainTexture as MovieTexture;
+		mtex.Play();
+		*/
+		((MovieTexture)GetComponent<Renderer> ().material.mainTexture).Play ();
+	}
+	/*
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!mtex.isPlaying)
+		{
+			print("movie playing status: " + mtex.isPlaying + ".");
+			mtex.Play();
+		}
+	}
+	*/
+}
---
+++ Base2/Assets/Scripts/SplashScreen/SplashScreenDelayed.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenDelayed : MonoBehaviour {
+	public float delayTime = 2;
+
+	IEnumerator Start(){
+		yield return new WaitForSeconds (delayTime);
+		//Application.LoadLevel ("Central");
+
+		//AutoFade.LoadLevel("Central" ,1,10,Color.black);
+		//levelname, fadeouttime, fadeintime, fadecolor
+	}
+}
