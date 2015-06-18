5d8cde0d7115f411b20e66b86998960f3e76b713
Floppi003 <f.peinsold@gmail.com>
2015-06-18 21:37:59 +0200

    Audio Files Level 2 + Level 3 imported

--- Base2/Assets/Scripts/Level2/AudioFilesLevel2.cs
+++ Base2/Assets/Scripts/Level2/AudioFilesLevel2.cs
@@ -9,6 +9,29 @@ public class AudioFilesLevel2 : MonoBehaviour {
 	public AudioClip buttonReleasedSound;
 	public AudioClip buttonGazedSound;
 
+
+	// Sonny Voice Clips
+	public AudioClip L2_Start;
+	public AudioClip[] R1_DrueckDenSchalter;
+	public AudioClip R1_Schalter;
+	public AudioClip R2_GuteAlteSchalter;
+	public AudioClip R2_Start;
+	public AudioClip R3_BlauerSchalter;
+	public AudioClip Bombenentschaerfer;
+	public AudioClip[] R3_Blau;
+	public AudioClip[] R3_Gelb;
+	public AudioClip[] R3_Gruen;
+	public AudioClip[] R3_Rot;
+	public AudioClip[] R3_Geschafft;
+	public AudioClip R3_Planaenderung;
+	public AudioClip R3_ZuerstBlau;
+	public AudioClip R4_MengeSchalter;
+	public AudioClip R4_ReiseGehtWeiter;
+	public AudioClip R4_SchonBesser;
+	public AudioClip R4_SoWirdDasNix;
+
+
+
 	// Use this for initialization
 	void Start () {
 	
---
+++ Base2/Assets/Scripts/Level3/AudioFilesLevel3.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class AudioFilesLevel3 : MonoBehaviour
+{
+
+	public AudioClip[] Bruecke;
+	public AudioClip BrueckeGeschafft;
+	public AudioClip MitteAugenkraft;
+	public AudioClip[] HeyVertrauen;
+	public AudioClip Stop;
+	public AudioClip Wow;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
