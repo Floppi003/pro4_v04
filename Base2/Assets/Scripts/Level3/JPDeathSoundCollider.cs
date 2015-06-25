bd330d5c43780aed17f025e2c0717bb874751ca1
Kaint <winter@schule-raumberg.at>
2015-06-25 15:32:49 +0200

    level 3 death sound collider JP

---
+++ Base2/Assets/Scripts/Level3/JPDeathSoundCollider.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class JPDeathSoundCollider : MonoBehaviour
+{
+	private AudioFilesLevel3 audioFiles;
+	
+	public void Start() {
+		this.audioFiles = GameObject.Find ("GM").GetComponent<AudioFilesLevel3> ();
+	}
+	
+	protected void OnTriggerEnter() {
+		AudioManager.instance.queueAudioClip (/*choose audioClip*/ null, 1);
+		Debug.LogError ("DEAD");
+	}
+}
+
